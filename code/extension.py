0bd4756499ca742fb317d6b30ff63c15f0fc358f
t-hanya <toshi.hanya@gmail.com>
2017-11-11 14:56:52 +0900

    Add extension to drop learning late

--- code/extension.py
+++ code/extension.py
@@ -3,6 +3,22 @@ from chainer.training import extension
 from chainer.training import extensions
 
 
+class LearningRateDrop(extension.Extension):
+    """Trainer extension to drop learning rate."""
+
+    def __init__(self, drop_ratio, attr='lr', optimizer=None):
+        self._drop_ratio = drop_ratio
+        self._attr = attr
+        self._optimizer = optimizer
+
+    def __call__(self, trainer):
+        opt =  self._optimizer or trainer.updater.get_optimizer('main')
+
+        lr = getattr(opt, self._attr)
+        lr *= self._drop_ratio
+        setattr(opt, self._attr, lr)
+
+
 class StepShift(extension.Extension):
 
     """Trainer extention to change an optimizer attribute on specified timing.
